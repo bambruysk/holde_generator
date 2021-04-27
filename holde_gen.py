d5df3b532e3af9e84f8b1bbfc062df9b76332198
bambruysk <bambruysk@gmail.com>
2021-04-27 12:53:57 +0000

    init connect

--- holde_gen.py
+++ holde_gen.py
@@ -1,7 +1,57 @@
+from google.oauth2 import service_account
+from googleapiclient.http import MediaIoBaseDownload,MediaFileUpload
+from googleapiclient.discovery import build
+import pprint
+import io
+
+from PIL import Image
+
+
+pp = pprint.PrettyPrinter(indent=4)
+SCOPES = ['https://www.googleapis.com/auth/drive']
+SERVICE_ACCOUNT_FILE = 'client_secret.json'
+
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+service = build('drive', 'v3', credentials=credentials)
+
+# results = service.files().list(pageSize=10,
+#                                fields="nextPageToken, files(id, name, mimeType)").execute()
+
+# pp.pprint(results)
+
+def getTemplate ():
+    print(" Start getting file")
+    results = service.files().list(pageSize=10,
+                               fields="nextPageToken, files(id, name, mimeType)").execute()
+    
+    pp.pprint(results)
+
+    file_id = '1kj-_IyuJVBc8nHgIZhN5G9VHcHYghGUu'
+    request = service.files().get_media(fileId=file_id)
+    filename = 'template.jpg'
+    fh = io.FileIO(filename, 'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print ("Download %d%%." % int(status.progress() * 100))
+
+    fh.close()
+
+
+
+
+
 def main():
-  pass
+    getTemplate()
+
+    
 
 
-if __name__ == 'main':
-  main()
 
+
+# if __name__ == 'main':
+#     main()
+
+main()
