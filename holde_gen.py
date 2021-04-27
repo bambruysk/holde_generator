ccec3237cf2aec983dbaca50f397076176115c1b
bambruysk <bambruysk@gmail.com>
2021-04-27 14:28:28 +0000

    add file saving

--- holde_gen.py
+++ holde_gen.py
@@ -3,8 +3,9 @@ from googleapiclient.http import MediaIoBaseDownload,MediaFileUpload
 from googleapiclient.discovery import build
 import pprint
 import io
+import sys
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -39,16 +40,55 @@ def getTemplate ():
 
     fh.close()
 
+def saveFileToGD(filename):
+    folder_id = '1O5psqszvUq-qG8ZwEFjS7nHnNLQiMJXw'
+    name = filename
+    file_path = filename
+    file_metadata = {
+                    'name': name,
+                    'parents': [folder_id]
+                }
+    media = MediaFileUpload(file_path, resumable=True)
+    r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    pp.pprint(r)
+
+class Holde():
+    def __init__(self, name, num, neighbours, corrupt, label):
+        self.name = name 
+        self.num = num
+        self.neighbours = neighbours
+        self.corrupt = corrupt
+        self.label =  label
 
 
+    def renderToFile(self, filename = ""):
+        if not name:
+            filename = self.name 
+            im = Image.open("template.jpg")
+        
+        draw = ImageDraw.Draw(im)
+        fnt = ImageFont.truetype("20351.otf", 120)
+        draw.text((100,100), "Поместье", font=fnt, fill=(255,255,255,255))
+
+        with open(filename, "wb") as res:
+            im.save(res, "JPEG")
 
 
 def main():
-    getTemplate()
-    
-    
+ #   getTemplate()
+    im = Image.open("template.jpg")
+    pp.pprint(im)
+    #size=2560x1777
+    width = 2560
+    height =  1777
+    draw = ImageDraw.Draw(im)
+    fnt = ImageFont.truetype("20351.otf", 100)
+    draw.text((width/2,300), "Поместье", font=fnt, fill=(0,0,0,255), align='center', anchor="ms")
 
+    with open("result.jpg", "wb") as res:
+        im.save(res, "JPEG")
 
+#    saveFileToGD("result.jpg")
 
 
 # if __name__ == 'main':
